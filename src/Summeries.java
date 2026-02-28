d2fb388782725adc469ee6c7cf1fb3865eb5bb4c
Ethan.Chiu <claratseng@Ethans-MacBook-Air.local>
2026-02-27 20:23:53 -0700

    Implement active member count

--- src/Summeries.java
+++ src/Summeries.java
@@ -5,3 +5,14 @@ public class Summaries {
     }
 
 }
+public static int activeMembers() {
+    int count = 0;
+
+    for (Object[] member : Data.membersById.values()) {
+        if ((boolean) member[Data.INDEX_ACTIVE]) {
+            count++;
+        }
+    }
+
+    return count;
+}
