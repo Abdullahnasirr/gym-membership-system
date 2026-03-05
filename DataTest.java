39345bfdc536b58d42dd987a3b509fd194241205
BrandonAung88 <brandon.aung@ucalgary.ca>
2026-03-05 14:47:36 -0700

    Add Junit Test skeleton Code for Data.java

---
+++ DataTest.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataTest {
+
+    @org.junit.jupiter.api.Test
+    void addMember() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void updateMemberInfo() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void recordCheckIn() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void recordPayment() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setActiveStatus() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllMembers() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMemberById() {
+    }
+}
