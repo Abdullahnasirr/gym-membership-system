a1c0959dfcc9f3cdbf2736f0eec77d2a88833098
BrandonAung88 <brandon.aung@ucalgary.ca>
2026-03-12 15:37:13 -0600

    Add setUp and addMember Testcase in DataTest.java

--- DataTest.java
+++ DataTest.java
@@ -1,32 +1,59 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DataTest {
-
-    @org.junit.jupiter.api.Test
-    void addMember() {
+    @BeforeEach
+    void setUp() {
+        Data.membersById.clear();
+        Data.memberIdList.clear();
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
+    void addMember() {
+        //Arrange
+        String name = "Brandon";
+        String type = Data.TYPE_MONTHLY;
+
+        //Add
+        int id = Data.addMember(name, type);
+        Object[] member = Data.getMemberById(id);
+
+        //Assert
+        assertNotNull(member);
+        assertEquals(1, Data.membersById.size());
+        assertEquals(1, Data.memberIdList.size());
+        assertTrue(Data.memberIdList.contains(id));
+        assertEquals(id, member[Data.INDEX_ID]);
+        assertEquals(name, member[Data.INDEX_NAME]);
+        assertEquals(type, member[Data.INDEX_TYPE]);
+        assertEquals(0, member[Data.INDEX_VISITS]);
+        assertEquals(0.0, (Double) member[Data.INDEX_TOTAL_PAID], 0.0001);
+        assertEquals(true, member[Data.INDEX_ACTIVE]);
+    }
+
+    @Test
     void updateMemberInfo() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void recordCheckIn() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void recordPayment() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void setActiveStatus() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getAllMembers() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getMemberById() {
     }
 }
