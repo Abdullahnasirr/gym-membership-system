0633de6f506bc757baf88f60b920b4325638de54
BrandonAung88 <brandon.aung@ucalgary.ca>
2026-03-18 16:02:53 -0600

    Add addMemberAndFindMemberById() test in AAA format to GymSystemTest.java

--- src/GymSystemTest.java
+++ src/GymSystemTest.java
@@ -23,9 +23,25 @@ class GymSystemTest {
         gymSystem = new GymSystem();
     }
 
+    /**
+     * Tests that a member can be added and retrieved by memberId.
+     * Verifies that the member is stored correctly and total count updates.
+     */
     @Test
     void addMemberAndFindMemberById() {
+        // Arrange
+        String id = gymSystem.generateMemberId();
+        Member member = new Member(id, "Brandon", "brandon@email.com",
+                "Calgary", "Monthly");
 
+        // Act
+        gymSystem.addMember(member);
+        Member found = gymSystem.findMemberById(id);
+
+        // Assert
+        assertNotNull(found);
+        assertEquals("Brandon", found.getFullName());
+        assertEquals(1, gymSystem.getTotalMembers());
     }
 
     @Test
